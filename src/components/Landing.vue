043b0ec08fe12c024e457eebf75e6eb2ea810783
Thanh Nguyen <thanhnguyen_2025@depauw.edu>
2023-10-05 21:04:27 -0400

    hotfix

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -10,11 +10,13 @@
         Cras sed efficitur dolor, in lacinia velit. Fusce nec dictum ex. Nullam bibendum nunc at neque ultrices, ut mollis metus blandit. Etiam hendrerit justo sem. Sed laoreet, nibh sit amet lacinia feugiat, nibh lacus facilisis ipsum, non pulvinar diam felis eu justo. In ante turpis, sagittis sit amet orci a, rhoncus placerat erat. Vestibulum finibus, augue et mollis imperdiet, eros mauris tempus quam, et ullamcorper augue ligula et leo. Duis ullamcorper dapibus mi ac fermentum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent tincidunt elit ultricies sapien blandit, at sollicitudin est placerat. Curabitur dolor tortor, convallis nec aliquam nec, ullamcorper nec turpis.
       </p1>
     </div>  
+    <!--
     <div id="center">
       <p1>
         Fusce in ante ac diam gravida mattis a ac dolor. Integer massa leo, dictum a ligula et, dictum volutpat diam. Suspendisse non enim in arcu consectetur aliquet. Nullam eu congue risus, quis elementum metus. Pellentesque posuere tincidunt dolor, id ultricies urna finibus ac. Pellentesque rutrum dui sed aliquam maximus. Sed vel enim nec mauris volutpat molestie non pretium mi. Aenean bibendum quam in sapien congue sollicitudin. Vivamus nec lorem sed diam dapibus commodo nec nec risus. Fusce in ante ac velit placerat scelerisque. Integer finibus dictum ante, quis porta diam condimentum sed. Nulla vel nisi felis. Donec non nunc augue. Quisque varius quis dui a dapibus. Morbi nisl tortor, porta eu volutpat quis, egestas non turpis.
       </p1>
     </div>
+    -->
   </div>
 </template>
 
